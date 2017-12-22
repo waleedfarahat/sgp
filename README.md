ea26b042411eda4ca8f6d657597348278e03eb29
Waleed Farahat <wfarahat@rethinkrobotics.com>
2017-12-22 09:50:52 -0500

    Updated refs in README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # stewart
 Analysis and control scripts for a Stewart mechanism
+
+
+Relevant papers:
+http://saba.kntu.ac.ir/eecd/taghirad/papers/2010_IROS_Reza_SGP.pdf
+http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.588.3600&rep=rep1&type=pdf
+https://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/19890014504.pdf
+https://pdfs.semanticscholar.org/6184/dad47787ce7f5a2ab1916111ee8e1f113aa5.pdf
+
--- sgp.m
+++ sgp.m
@@ -13,33 +13,39 @@ classdef sgp < handle
         end;
         
         function define(this)
-            angles = linspace(0, 2*pi*5/6, 6);
-            this.base.X = []; this.base.Y = []; this.base.Z = [];
-            for k = 1:length(angles)
-                currentVector = [real(exp(i*angles(k))), imag(exp(i*angles(k))), 0];
-                this.base.(['vector_', num2str(k)]) = currentVector;
+            % base
+            basePointAngles = linspace(0, 2*pi*5/6, 6);
+            this.base.X = []; this.base.Y = []; this.base.Z = []; this.base.vectors = {};
+            
+            for bpa = basePointAngles
+                currentVector = [real(exp(1i*bpa)), imag(exp(1i*bpa)), 0];
+                this.base.vectors{end+1} = currentVector;
                 this.base.X(end+1) = currentVector(1);
                 this.base.Y(end+1) = currentVector(2);
                 this.base.Z(end+1) = currentVector(3);
             end;
+            this.base.color = [0.2, 0.2, 0.5];
             
-            angles = linspace(2*pi/12, 2*pi*11/12, 6);
-            this.stage.X = []; this.stage.Y = []; this.stage.Z = [];
+            % Stage
+            stagePointAngles = linspace(2*pi/12, 2*pi*11/12, 6);
+            this.stage.X = []; this.stage.Y = []; this.stage.Z = []; this.stage.vectors = {};
             this.stage.offset = [0, 0, 1];
             this.stage.radius = 0.5;
-            for k = 1:length(angles)
-                currentVector = this.stage.offset + this.stage.radius*[real(exp(i*angles(k))), imag(exp(i*angles(k))), 0];
-                this.stage.(['vector_', num2str(k)]) = currentVector;
+            
+            for spa = stagePointAngles
+                currentVector = this.stage.offset + this.stage.radius*[real(exp(1i*spa)), imag(exp(1i*spa)), 0];
+                this.stage.vectors{end+1} = currentVector;
                 this.stage.X(end+1) = currentVector(1);
                 this.stage.Y(end+1) = currentVector(2);
                 this.stage.Z(end+1) = currentVector(3);
             end;
+            this.stage.color = [0.5, 0.2, 0.2];
         end;    
         
         function visualize(this)
             figure;
-            patch (this.base.X, this.base.Y, this.base.Z, [0.2, 0.2, 0.5]); hold on;
-            patch (this.stage.X, this.stage.Y, this.stage.Z, [0.5, 0.2, 0.2]); hold on;
+            patch (this.base.X, this.base.Y, this.base.Z, this.base.color); hold on;
+            patch (this.stage.X, this.stage.Y, this.stage.Z, this.stage.color); hold on;
             
             rotate3d on;
             axis equal;
